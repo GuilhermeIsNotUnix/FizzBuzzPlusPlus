b8018400264f81f8b5375315eb574556fb59c4f2
Guilherme Almino Carvalho <36975422+GuilhermeIsNotUnix@users.noreply.github.com>
2018-10-02 11:22:02 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,46 +1,37 @@
 #include <iostream>
-#include <unistd.h>
 
-int main(int argc, char const *argv[])
+#ifdef linux
+    #include <unistd.h>
+#elif defined(_WIN32) || defined(_WIN64)
+    #include <windows.h>
+#endif
+
+int main(void)
 {
-	int i = 0;
+    int i = 1;
 
-	//Loop infinito
-	while(1)
-	{
-		i++; //i = i + 1;
-		sleep(1); //Dorme por 1 segundo
+    while(true)
+    {
+        if(i % 3 != 0 && i % 5 != 0)
+            std::cout << i << std::endl;
+        else
+            if(i % 3 == 0 && i % 5 == 0)
+                std::cout << "FizzBuzz" << std::endl;
+            else
+            {
+                if(i % 3 == 0)
+                    std::cout << "Fizz" << std::endl;
+                if(i % 5 == 0)
+                    std::cout << "Buzz" << std::endl;
+            }
 
-		//Se não for multiplo de 3 e ao mesmo tempo não for multiplo de 5 imprime o valor de i
-		if(i % 3 != 0 && i % 5 != 0)
-		{
-			std::cout << i << std::endl;
-		}
+        #ifdef linux
+            sleep(1);
+        #elif defined(_WIN32) || defined(_WIN64)
+            Sleep(1000);
+        #endif
 
-		else
-		{
-			//Se for multiplo de 3 e ao mesmo tempo for multiplo de 5 imprime FizzBuzz
-			if(i % 3 == 0 && i % 5 == 0)
-			{
-				std::cout << "FizzBuzz" << std::endl;
-			}
-
-			else
-			{
-				//Se for multiplo de 3 imprime Fizz
-				if(i % 3 == 0)
-				{
-					std::cout << "Fizz" << std::endl;
-				}
-
-				//Se for multiplo de 5 imprime Buzz
-				if(i % 5 == 0)
-				{
-					std::cout << "Buzz" << std::endl;
-				}
-			}	
-		}
-	}
-
-	return 0;
+        i++;
+    }
+    return 0;
 }
